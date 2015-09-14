9be8088f1eaf5ee133f34229384e0af09714acdc
Florian Reuschel <reuschel@tozen.de>
2015-09-14 16:51:37 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # jquery-sequence
 Runs a callback on each element of a jQuery collection with a timer in between
+
+Use it like this:
+
+```
+$( ".selector" ).sequence( callback, 500 );
+```
+
+This will apply your `callback` function on each element of the collection with a delay of 500ms between each call.
+
+# Usage
+
+## Get started
+`.sequence()` takes 3 arguments:
+
+### callback
+The callback function to run over each element of the given collection. The `this` argument of the call will be the current element, just like it is in `jQuery.each`.
+
+### speed
+The interval between each call in milliseconds.
+
+### instantly
+An optional boolean, defaulting to `true`. Tells if the sequence should run as it is created or be `hold()` (see below) immediately to wait for further commands.
+
+
+## Control the sequence
+`.sequence()` returns a sequence controller object which is basically a `jQuery.Deferred()` loaded with some extra methods.
+You can run the familiar jQuery promise-like methods on the controller to determine when the sequence finishes.
+
+Additionally, there are the following methods appended:
+
+### .hold()
+Pauses the execution of the sequence instantly.
+
+### .release( [ when = "now" ] )
+Unpauses the execution of the sequence.
+The `when` parameter can either be
+- `"now"` to execute the next call on the sequence immediately (default)
+- `"delayed"` to wait a full turn and then run the callback or
+- `"remaining"` to resume exactly at the point where the execution was put on hold.
+
+### .runAll()
+Runs all remaining calls immediately.
+The promise will be resolved.
+
+### .clear()
+Puts the execution on hold and clears the sequence queue. The promise will be rejected.
+
+### .reset()
+Resets the sequence to be run from the beginning. Be aware that the actual sequence calls on the elements naturally can't be reverted like this.
---
+++ jquery.sequence.js
@@ -0,0 +1,165 @@
+;( function ( $ ) {
+
+$.fn.sequence = function( callback, interval, instantly ) {
+	if ( typeof instantly !== "boolean" ) instantly = true;
+
+	var done = false,
+		index = 0,
+		$elements = this,
+
+		promise = $.Deferred(),
+		methods = {},
+
+		settings = {
+			paused: !instantly,
+			interval: interval
+		},
+
+		currentRunTimeout = null,
+		currentRunTimestamp = 0,
+		remainingDelay = 0;
+
+	var reset = function () {
+		done = false;
+		index = 0;
+		promise = $.Deferred();
+		methods = {};
+
+		$elements
+			.data( "sequence-queued", true )
+			.data( "sequence-done", false );
+
+		// really dirty, I know
+		for ( var name in methods ) {
+			promise[ name ] = methods[ name ];
+		}
+	};
+
+
+	// modify promise object
+	// get / set interval
+	methods.getInterval = function() {
+		return settings.interval;
+	};
+	methods.setInterval = function( interval ) {
+		if ( done ) return;
+
+		settings.interval = interval;
+	};
+
+
+	// hold / release
+	methods.hold = function() {
+		if ( done ) return;
+
+		remainingDelay = 1 - ( ( Date.now() - currentRunTimestamp ) / settings.interval );
+		clearTimeout( currentRunTimeout );
+		settings.paused = true;
+	};
+	methods.release = function( when ) {
+		if ( done ) return;
+		if ( typeof when === "undefined" ) when = "now";
+
+		settings.paused = false;
+		chain( when );
+	};
+
+
+	// run all at once
+	methods.runAll = function() {
+		if ( done ) return;
+
+		promise.hold();
+
+		var i = index,
+			element;
+
+		while ( element = $elements.get( i ) ) {
+			callback.apply( element );
+			$( element ).data( "sequence-done" , true );
+			i++;
+		}
+
+		tidyUp( true );
+	};
+
+
+	// stop the sequence and 
+	methods.clear = function() {
+		if ( done ) return;
+
+		promise.hold();
+		tidyUp( false );
+	};
+
+	// resets the sequence
+	methods.reset = function() {
+		reset();
+	};
+
+
+	reset();
+
+
+	// clean up in here and handle our promise
+	var tidyUp = function( successful ) {
+		if ( typeof successful !== "boolean" ) successful = true;
+
+		$elements
+			.removeData( "sequence-queued" )
+			.removeData( "sequence-done" );
+
+		if ( successful )
+			promise.resolve();
+		else
+			promise.reject();
+
+		done = true;
+	};
+
+
+	// the actual sequence runner
+	var chain = function( when ) {
+		if ( settings.paused ) return;
+		if ( typeof when === "undefined" ) when = "now";
+
+		var interval = settings.interval;
+
+		switch ( when ) {
+		case "remaining":
+			interval *= remainingDelay;
+			break;
+
+		case "now":
+			var element = $elements.get( index );
+			if ( typeof element === "undefined" ) return;
+
+			callback.apply( element );
+			$( element ).data( "sequence-done", true );
+			index++;
+
+			if ( index === $elements.length ) {
+				tidyUp( true );
+				return;
+			}
+			break;
+		}
+
+		currentRunTimestamp = Date.now();
+		currentRunTimeout = setTimeout( function() {
+			chain();
+		}, interval );
+	};
+
+	// setTimeout is used before the first call to wait for a page to load completely
+	// else the first call of the sequence would be run and then stutter to wait for the page to finish loading
+	if ( instantly ) {
+		setTimeout( function () {
+			chain();
+		} );
+	}
+
+	return promise;
+};
+
+} )( jQuery );
